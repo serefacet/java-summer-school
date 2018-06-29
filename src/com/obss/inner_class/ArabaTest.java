100565447130143bad89c22ad96c4eef673de685
serefacet <serefacet@users.noreply.github.com>
2018-06-29 14:16:52 +0300

    OBSS Java Summer School Day 5

---
+++ src/com/obss/inner_class/ArabaTest.java
@@ -0,0 +1,11 @@
+package com.obss.inner_class;
+
+public class ArabaTest {
+
+    public static void main(String[] args) {
+        Araba araba = new Araba();
+        Araba.Fren fren = araba.new Fren();
+
+
+    }
+}
